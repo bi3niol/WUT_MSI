22c20db7cfc1b03cbd376542555a7e419c3c9333
bellac <c.bella@student.mini.pw.edu.pl>
2017-11-17 02:01:14 +0100

    Interface for Fuzzy Functions

--- WUT_MSI.ModelsLib/FuzzyFunctions.cs
+++ WUT_MSI.ModelsLib/FuzzyFunctions.cs
@@ -10,15 +10,20 @@ namespace WUT_MSI.Models
 {
     public static class FuzzyFunctions
     {
+        public interface FuncFuzzy
+        {
+            double Invoke(ICountry country);
+        }
         
-        public class DistanceFuzzy
+        public class DistanceFuzzy : FuncFuzzy
         {
             public double Invoke(ICountry country)
             {
                 return country.Distance / FuzzyProps.Instance.MaxDistance;
             }
         }
-        public class AreaFuzzy
+
+        public class AreaFuzzy : FuncFuzzy
         {
             public double Invoke(ICountry country)
             {
