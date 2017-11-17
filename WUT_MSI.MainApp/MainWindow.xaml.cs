49c3e3ce8d1a01b73d39e6bd243f1d4694d01466
bellac <c.bella@student.mini.pw.edu.pl>
2017-11-17 14:41:39 +0100

    Change interface for WPF (Evaluator)

--- WUT_MSI.MainApp/MainWindow.xaml.cs
+++ WUT_MSI.MainApp/MainWindow.xaml.cs
@@ -1,37 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using WUT_MSI.MainApp.Managers;
-using WUT_MSI.Models;
 using WUT_MSI.Models.classes;
+using WUT_MSI.ModelsLib.classes;
 using WUT_MSI.ModelsLib.classes.helpers;
-using WUT_MSI.ModelsLib.interfaces;
 
 namespace WUT_MSI.MainApp
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow: Window
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: Window
 	{
-        private IQuestionGetter<Country> questionGetter;
+        private Evaluator<Country> evaluator;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-            questionGetter = new CountryQuestionGetter(QuestionGenerator.GetQuestions());
+            evaluator = new Evaluator<Country>(new CountryQuestionGetter(QuestionGenerator.GetQuestions()));
         }
 	}
 }
