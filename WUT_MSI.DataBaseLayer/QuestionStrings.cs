a23ac4feddaa835e7967108496167c7505019e48
Bi3NioL <bi3niol@gmail.com>
2017-12-19 22:34:55 +0100

    questionStrings, Adnotations

---
+++ WUT_MSI.DataBaseLayer/QuestionStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUT_MSI.DataBaseLayer
+{
+    public sealed class QuestionStrings
+    {
+        public const string Distance = "Jak daleko znajduje się państwo?";
+        public const string Climate = "Jaka panuje temperatura?";
+        public const string Area = "Jak duży jest kraj?";
+        public const string Development = "W jakim stopniu kraj jest rozwinięty?";
+        public const string Rains = "Jak dużo opadów występuje?";
+        public const string Safety = "Jaki jest poziom bezpieczeństwa kraju?";
+        public const string Medicine = "Jaki jest poziom medycyny w kraju?";
+        public const string Population = "Jak dużo ludności mieszka w kraju?";
+        public const string Density = "Jaka jest gęstość zaludnienia?";
+        public const string Jet = "Jak duży chcesz mieć jetlag?";
+        public const string Sea = "Czy ma dostęp do morza lub oceanu?";
+        public const string Mountain = "Czy występują góry?";
+    }
+}
--- WUT_MSI.DataBaseLayer/Tables/DbCountryAttributes.cs
+++ WUT_MSI.DataBaseLayer/Tables/DbCountryAttributes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -10,30 +11,42 @@ namespace WUT_MSI.DataBaseLayer.Tables
 {
     public class DbCountryAttributes: Entity<long>
     {
+        [DisplayName("Kraj")]
         public virtual DbCountry Country { get; set; }
-
+        [DisplayName(QuestionStrings.Distance)]
         public virtual DbAttributeValue DistanceAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Climate)]
         public virtual DbAttributeValue ClimateAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Area)]
         public virtual DbAttributeValue AreaAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Development)]
         public virtual DbAttributeValue DevelopmentAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Rains)]
         public virtual DbAttributeValue RainsAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Safety)]
         public virtual DbAttributeValue SafetyAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Medicine)]
         public virtual DbAttributeValue MedicineAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Population)]
         public virtual DbAttributeValue PopulationAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Density)]
         public virtual DbAttributeValue DensityAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Jet)]
         public virtual DbAttributeValue JetAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Sea)]
         public virtual DbAttributeValue SeaAttributeValue { get; set; }
 
+        [DisplayName(QuestionStrings.Mountain)]
         public virtual DbAttributeValue MountainAttributeValue { get; set; }
     }
 }
